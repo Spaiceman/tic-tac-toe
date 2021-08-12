d158b9fa4b27b375800da6ef492cea21424624bf
Spaiceman <76124405+Spaiceman@users.noreply.github.com>
2021-08-13 01:43:31 +0300

    feat: end task

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,123 @@
 class TicTacToe {
-    constructor() {
+  constructor() {
+    this.count = 0;
+    this.currentPlayer = "x";
+    this.winner = null;
+    this.result = null;
 
+    this.matrix = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ];
+  }
+
+  getCurrentPlayerSymbol() {
+    return this.currentPlayer;
+  }
+
+  nextTurn(rowIndex, columnIndex) {
+    if (this.matrix[rowIndex][columnIndex] === null) {
+      this.matrix[rowIndex][columnIndex] = this.currentPlayer;
+      this.currentPlayer === "x"
+        ? (this.currentPlayer = "o")
+        : (this.currentPlayer = "x");
     }
+    this.matrixSearch();
+  }
 
-    getCurrentPlayerSymbol() {
-
+  isFinished() {
+    if (this.result === "draw" || this.winner != null) {
+      return true;
     }
+    return false;
+  }
 
-    nextTurn(rowIndex, columnIndex) {
+  getWinner() {
+    return this.winner;
+  }
 
+  noMoreTurns() {
+    for (let arr of this.matrix) {
+      if (arr.includes(null)) {
+        return false;
+      }
     }
+    return true;
+  }
 
-    isFinished() {
-
+  isDraw() {
+    return this.result === "draw";
+  }
+  getFieldValue(rowIndex, colIndex) {
+    return this.matrix[rowIndex][colIndex];
+  }
+  matrixSearch() {
+    if (
+      this.matrix[0][0] === this.matrix[0][1] &&
+      this.matrix[0][1] === this.matrix[0][2] &&
+      this.matrix[0][0] != null
+    ) {
+      this.winner = this.matrix[0][0];
+    } else if (
+      this.matrix[1][0] === this.matrix[1][1] &&
+      this.matrix[1][1] === this.matrix[1][2] &&
+      this.matrix[1][0] != null
+    ) {
+      this.winner = this.matrix[1][0];
+    } else if (
+      this.matrix[2][0] === this.matrix[2][1] &&
+      this.matrix[2][1] === this.matrix[2][2] &&
+      this.matrix[2][0] != null
+    ) {
+      this.winner = this.matrix[2][0];
+      /*row*/
+    } else if (
+      this.matrix[0][0] === this.matrix[1][0] &&
+      this.matrix[2][0] === this.matrix[0][0] &&
+      this.matrix[0][0] != null
+    ) {
+      this.winner = this.matrix[0][0];
+    } else if (
+      this.matrix[0][1] === this.matrix[1][1] &&
+      this.matrix[2][1] === this.matrix[0][1] &&
+      this.matrix[0][1] != null
+    ) {
+      this.winner = this.matrix[0][1];
+    } else if (
+      this.matrix[0][2] === this.matrix[1][2] &&
+      this.matrix[2][2] === this.matrix[0][2] &&
+      this.matrix[0][2] != null
+    ) {
+      this.winner = this.matrix[0][2];
+    } else if (
+      this.matrix[0][0] === this.matrix[1][1] &&
+      this.matrix[1][1] === this.matrix[2][2] &&
+      this.matrix[0][0] != null
+    ) {
+      this.winner = this.matrix[0][0];
+    } else if (
+      this.matrix[2][0] === this.matrix[1][1] &&
+      this.matrix[1][1] === this.matrix[0][2] &&
+      this.matrix[2][0] != null
+    ) {
+      this.winner = this.matrix[2][0];
     }
-
-    getWinner() {
-
+    if (this.winner === null) {
+      for (let i = 0; i < this.matrix.length; i++) {
+        if (this.matrix[i].includes(null)) {
+          this.count++;
+        }
+      }
+      if (this.count === 0) {
+        this.result = "draw";
+        this.turns = true;
+      } else {
+        this.count = 0;
+      }
     }
-
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+  }
 }
 
 module.exports = TicTacToe;
+console.log();
